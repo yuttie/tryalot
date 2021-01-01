4aa0570f9f8b91210e695c8dc5e0be6f05a6723d
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2021-01-01 23:29:25 +0900

    Add another test case for testing a pipeline with different conditions

--- tests/test_tryalot.py
+++ tests/test_tryalot.py
@@ -69,3 +69,35 @@ def test_simple_pipeline(tmp_path):
         return w + '|' + w
 
     assert ctx.compute('p3_output') == 'output1+output2+output3|output1+output2+output3'
+
+
+def test_simple_pipeline_with_condition(tmp_path):
+    @tryalot.module(input=[], output=['p1_output1', 'p1_output2', 'p1_output3'])
+    def process1(*, upper_case):
+        """This is the docstring for process1."""
+        print('Executing process1')
+        if upper_case:
+            return 'OUTPUT1', 'OUTPUT2', 'OUTPUT3'
+        else:
+            return 'output1', 'output2', 'output3'
+
+    class Process2(tryalot.Module):
+        """This is the docstring for process2."""
+        def __init__(self):
+            super().__init__(
+                ['p1_output1', 'p1_output2', 'p1_output3'],
+                ['p2_output'])
+
+        def execute(self, x, y, z, *, glue):
+            print('Executing process2')
+            return glue.join([x, y, z])
+
+    ctx = tryalot.Context(tmp_path)
+    ctx.register_modules(process1, Process2())
+
+    @ctx.module(['p2_output'], ['p3_output'])
+    def process3(w):
+        return w + '|' + w
+
+    assert ctx.compute('p3_output', dict(process1=dict(upper_case=True), Process2=dict(glue='--'))) == 'OUTPUT1--OUTPUT2--OUTPUT3|OUTPUT1--OUTPUT2--OUTPUT3'
+    assert ctx.compute('p3_output', dict(process1=dict(upper_case=False), Process2=dict(glue='++'))) == 'output1++output2++output3|output1++output2++output3'
