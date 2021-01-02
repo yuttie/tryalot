41095b74a54fb54ab8eb998e034f14f6a386cbb2
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2021-01-02 23:50:17 +0900

    Allow one to override existing producer

--- tryalot/__init__.py
+++ tryalot/__init__.py
@@ -105,8 +105,6 @@ class Context:
                 warnings.warn(f'Module "{mod.name}" is not an instance of Module', RuntimeWarning)
             self._modules.add(mod)
             for name in mod.output_names:
-                if name in self._producer:
-                    raise f'Producer for "{name}" is already registered'
                 self._producer[name] = mod
 
     def module(self, input, output):
