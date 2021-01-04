bfc97a3e6f87176635e5f486e35c58e2e28d2b51
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2021-01-04 17:13:07 +0900

    Warn if one try to register two or more producers for the same product

--- tryalot/__init__.py
+++ tryalot/__init__.py
@@ -105,6 +105,8 @@ class Context:
                 warnings.warn(f'Module "{mod.name}" is not an instance of Module', RuntimeWarning)
             self._modules.add(mod)
             for name in mod.output_names:
+                if name in self._producer:
+                    warnings.warn(f'Producer for "{name}" is already registered')
                 self._producer[name] = mod
 
     def module(self, input, output):
